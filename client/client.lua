e173e83fc4489abf5c3ac83824034792ed46d141
Benn3t <109836520+Bennn3t@users.noreply.github.com>
2025-05-28 20:25:18 +0100

    Improve readability and linting

--- client/client.lua
+++ client/client.lua
@@ -1,139 +1,106 @@
-
 if Config.Framework == 'QBCore' then
     Framework = exports['qb-core']:GetCoreObject()
 elseif Config.Framework == 'BJCore' then
     Framework = nil
     CreateThread(function() 
-        while Framework == nil do
+        while not Framework do
             TriggerEvent("BJCore:GetObject", function(obj) Framework = obj end)
             Wait(200)
         end
     end)
 else
-    local message = 'Error in Config.Framework, please review and try again.'
-    TriggerServerEvent('antideath:server:configerror', message)
+    TriggerServerEvent('antideath:server:configerror', 'Error in Config.Framework, please review and try again.')
 end
 
-
+-- Game Event: Entity Damage
 AddEventHandler('gameEventTriggered', function(event, data)
-    if event == "CEventNetworkEntityDamage" then
-        local Player = Framework.Functions.GetPlayerData()
-        local playerPed = PlayerPedId()
-        local victim = NetworkGetPlayerIndexFromPed(data[1])
-        local sourceOfDeath = GetPedSourceOfDeath(playerPed)
+    if event ~= "CEventNetworkEntityDamage" then return end
 
-        if victim == PlayerId() then
-            ped = PlayerPedId()
-            if Player.metadata["injail"] > 0 then
-                if Config.ShowDebugPrints then print('Player has jail time') end
-                if GetEntityHealth(ped) < Config.MinHealth then
-                    if Config.ShowDebugPrints then print('Players health is below Config.MinHealth') end
-                    while GetEntitySpeed(ped) > 0.5 or IsPedRagdoll(ped) or IsPedArmed(ped) do
-                        Wait(10)
-                    end
-                    if Config.ShowDebugPrints then print('Players ragdolled or speed above 0.5 or armed') end
-                    if Config.CheckPrisonGrounds then
-                        if InPrison then
-                            if sourceOfDeath ~= 0 and IsEntityAPed(sourceOfDeath) then
-                                if not IsPedAPlayer(sourceOfDeath) then
-                                    if Config.ShowDebugPrints then print('Killed by an NPC') end
-                                    if Config.NPCAttack then
-                                        Wait(150)
-                                        TriggerEvent('hospital:client:Revive')
-                                        if Config.ShowDebugPrints then print('Revivied') end
-                                    end
-                                else
-                                    if Config.ShowDebugPrints then print('Killed by a player') end
-                                    if not Config.NPCAttack then
-                                        Wait(150)
-                                        TriggerEvent('hospital:client:Revive')
-                                        if Config.ShowDebugPrints then print('Revivied') end
-                                    end
-                                end
-                            else
-                                if Config.ShowDebugPrints then print('Killed yourself') end
-                                if Config.FallDamage then
-                                    Wait(150)
-                                    TriggerEvent('hospital:client:Revive')
-                                    if Config.ShowDebugPrints then print('Revivied') end
-                                end
-                            end
-                        end
-                    else
-                        if sourceOfDeath ~= 0 and IsEntityAPed(sourceOfDeath) then
-                            if not IsPedAPlayer(sourceOfDeath) then
-                                if Config.ShowDebugPrints then print('Killed by an NPC') end
-                                if Config.NPCAttack then
-                                    Wait(150)
-                                    TriggerEvent('hospital:client:Revive')
-                                    if Config.ShowDebugPrints then print('Revivied') end
-                                end
-                            else
-                                if Config.ShowDebugPrints then print('Killed by a player') end
-                                if not Config.NPCAttack then
-                                    Wait(150)
-                                    TriggerEvent('hospital:client:Revive')
-                                    if Config.ShowDebugPrints then print('Revivied') end
-                                end
-                            end
-                        else
-                            if Config.ShowDebugPrints then print('Killed yourself') end
-                            if Config.FallDamage then
-                                Wait(150)
-                                TriggerEvent('hospital:client:Revive')
-                                if Config.ShowDebugPrints then print('Revivied') end
-                            end
-                        end
-                    end
-                end
-            end
+    local Player = Framework.Functions.GetPlayerData()
+    local ped = PlayerPedId()
+    local victim = NetworkGetPlayerIndexFromPed(data[1])
+    local sourceOfDeath = GetPedSourceOfDeath(ped)
+
+    if victim ~= PlayerId() or Player.metadata["injail"] <= 0 then return end
+
+    if Config.ShowDebugPrints then print('Player has jail time') end
+    if GetEntityHealth(ped) >= Config.MinHealth then return end
+
+    if Config.ShowDebugPrints then print('Players health is below Config.MinHealth') end
+
+    while GetEntitySpeed(ped) > 0.5 or IsPedRagdoll(ped) or IsPedArmed(ped) do
+        Wait(10)
+    end
+
+    if Config.ShowDebugPrints then print('Players ragdolled or speed above 0.5 or armed') end
+
+    local function handleRevive(shouldRevive)
+        if shouldRevive then
+            Wait(150)
+            TriggerEvent('hospital:client:Revive')
+            if Config.ShowDebugPrints then print('Revived') end
         end
+    end
+
+    local isNPC = sourceOfDeath ~= 0 and IsEntityAPed(sourceOfDeath) and not IsPedAPlayer(sourceOfDeath)
+
+    local function evaluateDeath()
+        if isNPC then
+            if Config.ShowDebugPrints then print('Killed by an NPC') end
+            handleRevive(Config.NPCAttack)
+        elseif sourceOfDeath ~= 0 and IsEntityAPed(sourceOfDeath) then
+            if Config.ShowDebugPrints then print('Killed by a player') end
+            handleRevive(not Config.NPCAttack)
+        else
+            if Config.ShowDebugPrints then print('Killed yourself') end
+            handleRevive(Config.FallDamage)
+        end
+    end
+
+    if Config.CheckPrisonGrounds then
+        if InPrison then evaluateDeath() end
+    else
+        evaluateDeath()
     end
 end)
 
-
+-- Prison Zone Handling
 if Config.CheckPrisonGrounds then 
     InPrison = false
-	function CreatePrisonZone()
-		PrisonZone = PolyZone:Create(Config.PolyZonePoints, Config.PolyzoneOptions)
-		ManagePrisonZone()
-	end
-    if Config.Framework == 'QBCore' then
-        RegisterNetEvent("QBCore:Client:OnPlayerLoaded")
-        AddEventHandler("QBCore:Client:OnPlayerLoaded", function()
-            if PrisonZone == nil then
-                CreatePrisonZone()
-            end
-        end)
-    elseif Config.Framework == 'BJCore' then
-        RegisterNetEvent("BJCore:Client:OnPlayerLoaded")
-        AddEventHandler("BJCore:Client:OnPlayerLoaded", function()
-            if PrisonZone == nil then
-                CreatePrisonZone()
+
+    local function ManagePrisonZone()
+        PrisonZone:onPlayerInOut(function(isPointInside, point)
+            InPrison = isPointInside
+            if Config.ShowDebugPrints then print(isPointInside and "Entered Zone" or "Left Zone") end
+            if not isPointInside then
+                local Player = Framework.Functions.GetPlayerData()
+                if Player.metadata["injail"] > 0 then
+                    SetEntityCoords(PlayerPedId(), Config.TeleportLocation)
+                end
             end
         end)
     end
 
-	function ManagePrisonZone()
-		PrisonZone:onPlayerInOut(function(isPointInside, point)
-			if isPointInside then
-                if Config.ShowDebugPrints then print("Entered Zone") end
-                InPrison = true
-			else
-                InPrison = false
-                local Player = Framework.Functions.GetPlayerData()
-				if Player.metadata["injail"] > 0 then
-					SetEntityCoords(PlayerPedId(), Config.TeleportLocation)
-				end
-                if Config.ShowDebugPrints then print("Left Zone") end
-			end
-		end)
-	end
+    local function CreatePrisonZone()
+        PrisonZone = PolyZone:Create(Config.PolyZonePoints, Config.PolyzoneOptions)
+        ManagePrisonZone()
+    end
+
+    local function onPlayerLoaded()
+        if not PrisonZone then
+            CreatePrisonZone()
+        end
+    end
+
+    if Config.Framework == 'QBCore' then
+        RegisterNetEvent("QBCore:Client:OnPlayerLoaded", onPlayerLoaded)
+    elseif Config.Framework == 'BJCore' then
+        RegisterNetEvent("BJCore:Client:OnPlayerLoaded", onPlayerLoaded)
+    end
+
     AddEventHandler('onResourceStart', function(resource)
-        if GetCurrentResourceName() == resource then
-            if PrisonZone == nil then
-                CreatePrisonZone()
-            end
+        if GetCurrentResourceName() == resource and not PrisonZone then
+            CreatePrisonZone()
         end
     end)
 end
